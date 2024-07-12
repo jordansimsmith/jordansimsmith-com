586221a064f41c699a58f5e1279d012d738b0e21
Jordan Sim-Smith <jordansimsmith@canva.com>
2024-07-12 12:33:11 +1200

    Configure dynamodb client in tests

--- immersion_tracker_api/src/test/java/com/jordansimsmith/immersiontracker/MainIntegrationTest.java
+++ immersion_tracker_api/src/test/java/com/jordansimsmith/immersiontracker/MainIntegrationTest.java
@@ -5,13 +5,19 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
 
 import java.io.IOException;
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 public class MainIntegrationTest {
+
+    private static final int DYNAMODB_PORT = 8000;
 
     // TODO: move to DynamoDbContainer in separate bazel target
     static GenericContainer<?> getDynamoDb() {
@@ -31,11 +37,28 @@ public class MainIntegrationTest {
     }
 
     @Container
-    public GenericContainer<?> dynamodb = getDynamoDb();
+    GenericContainer<?> dynamodb = getDynamoDb();
 
     @Test
-    void test() {
+    void test() throws Exception {
         assertThat(dynamodb.isRunning()).isTrue();
         assertThat(dynamodb.getHost()).isEqualTo("localhost");
+
+        var endpoint = new URI("http://%s:%d".formatted(dynamodb.getHost(), dynamodb.getMappedPort(DYNAMODB_PORT)));
+
+        var client = DynamoDbClient.builder().endpointOverride(endpoint).build();
+        var enhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(client).build();
+
+        assertThat(client.listTables().tableNames()).isEmpty();
+
+        var req = CreateTableRequest.builder()
+                .tableName("my_table")
+                .keySchema(KeySchemaElement.builder().keyType(KeyType.HASH).attributeName("pk").build())
+                .attributeDefinitions(AttributeDefinition.builder().attributeName("pk").attributeType(ScalarAttributeType.S).build())
+                .provisionedThroughput(ProvisionedThroughput.builder().readCapacityUnits(1L).writeCapacityUnits(1L).build())
+                .build();
+        var res = client.createTable(req);
+
+        assertThat(client.listTables().tableNames()).contains("my_table");
     }
 }
