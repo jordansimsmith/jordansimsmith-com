b4cdc48b4dcfa121716ce8a4ced3ab1c23e72fd5
Jordan Sim-Smith <jordansimsmith@canva.com>
2024-10-28 11:41:12 +0900

    Configure python support for the sync script

---
+++ immersion_tracker_api/script/sync_episodes.py
@@ -0,0 +1,7 @@
+import time
+
+print("Hello, world!")
+time.sleep(1)
+n = input("Pick a number:\n")
+time.sleep(1)
+print("You picked " + n)
--- immersion_tracker_api/src/test/java/com/jordansimsmith/immersiontracker/ImmersionTrackerE2ETest.java
+++ immersion_tracker_api/src/test/java/com/jordansimsmith/immersiontracker/ImmersionTrackerE2ETest.java
@@ -2,6 +2,9 @@ package com.jordansimsmith.immersiontracker;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -14,7 +17,25 @@ public class ImmersionTrackerE2ETest {
       new ImmersionTrackerContainer();
 
   @Test
-  void test() {
+  void scriptShouldSyncEpisodes() throws Exception {
     assertThat(immersionTrackerContainer.isRunning()).isTrue();
+
+    var processBuilder = new ProcessBuilder("immersion_tracker_api/sync-episodes-script.py");
+    processBuilder.redirectErrorStream(true);
+
+    var process = processBuilder.start();
+    var input = process.getOutputStream();
+    input.write("73\n".getBytes());
+    input.flush();
+    int exitCode = process.waitFor();
+    assertThat(exitCode).isEqualTo(0);
+
+    var outputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    var output = outputReader.lines().collect(Collectors.joining("\n"));
+    assertThat(output)
+        .isEqualTo("""
+        Hello, world!
+        Pick a number:
+        You picked 73""");
   }
 }
