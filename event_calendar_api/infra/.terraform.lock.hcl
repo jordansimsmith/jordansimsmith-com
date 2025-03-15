f390444aa8fb78a2114e869073e87671de204dd3
Jordan Sim-Smith <jordansimsmith@canva.com>
2025-03-15 17:48:23 +1300

    Deploy event calendar

---
+++ event_calendar_api/infra/.terraform.lock.hcl
@@ -0,0 +1,44 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.91.0"
+  constraints = "~> 5.61"
+  hashes = [
+    "h1:bZ3AM8Qd1veQoUJqOb7OVp/ElmLg/hkz3bHL2DZZ5Tk=",
+    "zh:03ee14261b25aee94c735ed6ef7cce47900ab7bdf462335432ca034d0ba74ca2",
+    "zh:32a3759049c9c2cd041d1257cf16cb90a5ce586e1d0a6fe5f8ebd0ec1ba8e071",
+    "zh:334db69bc6d8643ec4ea432f0e54e851c2394bbe889cca29ca5029db0e4699e8",
+    "zh:39957a4a900f100ea8d85845a42164a44c9efea8559a9e74ab4f6a1193e20c3e",
+    "zh:8831396c764815eb367601a522c51c2e9e8fc38bcaa5f5e83f21de771778e9ba",
+    "zh:8e71ab68c27f909892a063f845d92faa487297ad9bbc67c77a67194e509781e6",
+    "zh:944df1084a7ea37a4feea0ee6654fd15891ef4829c5453bc149ffbcc0ab9bad7",
+    "zh:964391527624f2e66a4eb387ad0a30a1b67a896e9395b6d01353f2572723ea03",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:bb956c660185161e8ce1ed1dbf187c9f549d1779673fe798211dd5b02b98c737",
+    "zh:c237199ca8cd88f4aab4c673f848c77670b90d98a484fef6bcd31a71ff63d9b9",
+    "zh:c7522f6072f8ba29f4a6d0f994eda8a381ed2f4a41dbe44c4d989c44852cfe63",
+    "zh:d412a852ced01433c44f222952b60974f7c297a8a21bef62c9a627b050084134",
+    "zh:e420266b772041fa89e5868594ed21c8c3090d76b3ec0262054f768a7807f5a7",
+    "zh:ecfcc7844e9e01123920a4b0e667a4688654e3f22f00890ec80ddd78e7312eda",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/external" {
+  version = "2.3.4"
+  hashes = [
+    "h1:cCabxnWQ5fX1lS7ZqgUzsvWmKZw9FA7NRxAZ94vcTcc=",
+    "zh:037fd82cd86227359bc010672cd174235e2d337601d4686f526d0f53c87447cb",
+    "zh:0ea1db63d6173d01f2fa8eb8989f0809a55135a0d8d424b08ba5dabad73095fa",
+    "zh:17a4d0a306566f2e45778fbac48744b6fd9c958aaa359e79f144c6358cb93af0",
+    "zh:298e5408ab17fd2e90d2cd6d406c6d02344fe610de5b7dae943a58b958e76691",
+    "zh:38ecfd29ee0785fd93164812dcbe0664ebbe5417473f3b2658087ca5a0286ecb",
+    "zh:59f6a6f31acf66f4ea3667a555a70eba5d406c6e6d93c2c641b81d63261eeace",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:ad0279dfd09d713db0c18469f585e58d04748ca72d9ada83883492e0dd13bd58",
+    "zh:c69f66fd21f5e2c8ecf7ca68d9091c40f19ad913aef21e3ce23836e91b8cbb5f",
+    "zh:d4a56f8c48aa86fc8e0c233d56850f5783f322d6336f3bf1916e293246b6b5d4",
+    "zh:f2b394ebd4af33f343835517e80fc876f79361f4688220833bc3c77655dd2202",
+    "zh:f31982f29f12834e5d21e010856eddd19d59cd8f449adf470655bfd19354377e",
+  ]
+}
---
+++ event_calendar_api/infra/main.tf
@@ -0,0 +1,266 @@
+terraform {
+  backend "s3" {
+    bucket = "jordansimsmith-terraform"
+    key    = "event_calendar_api/infra/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.61"
+    }
+  }
+
+  required_version = ">= 1.9.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+
+  default_tags {
+    tags = {
+      application_id = local.application_id
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      application_id = local.application_id
+    }
+  }
+}
+
+locals {
+  application_id = "event_calendar_api"
+
+  lambdas = {
+    get_calendar_subscription = {
+      target  = "//event_calendar_api:get-calendar-subscription-handler_deploy.jar"
+      handler = "com.jordansimsmith.eventcalendar.GetCalendarSubscriptionHandler"
+    }
+    update_events = {
+      target  = "//event_calendar_api:update-events-handler_deploy.jar"
+      handler = "com.jordansimsmith.eventcalendar.UpdateEventsHandler"
+    }
+  }
+
+  endpoints = {
+    get_calendar_subscription = {
+      path   = "calendar"
+      method = "GET"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "event_calendar_table" {
+  name         = "event_calendar"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "pk"
+  range_key    = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  deletion_protection_enabled = true
+}
+
+data "aws_iam_policy_document" "lambda_sts_allow_policy_document" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "${local.application_id}_lambda_exec"
+  assume_role_policy = data.aws_iam_policy_document.lambda_sts_allow_policy_document.json
+}
+
+data "aws_iam_policy_document" "lambda_dynamodb_allow_policy_document" {
+  statement {
+    effect = "Allow"
+
+    resources = [
+      aws_dynamodb_table.event_calendar_table.arn
+    ]
+
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:GetItem",
+      "dynamodb:BatchGetItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:ConditionCheckItem",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_dynamodb" {
+  name   = "${local.application_id}_lambda_dynamodb"
+  policy = data.aws_iam_policy_document.lambda_dynamodb_allow_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+data "external" "handler_location" {
+  for_each = local.lambdas
+
+  program = ["bash", "../../tools/terraform/resolve_location.sh"]
+
+  query = {
+    target = each.value.target
+  }
+}
+
+resource "aws_lambda_function" "lambda" {
+  for_each = local.lambdas
+
+  filename         = data.external.handler_location[each.key].result.location
+  function_name    = "${local.application_id}_${each.key}"
+  role             = aws_iam_role.lambda_role.arn
+  source_code_hash = filebase64sha256(data.external.handler_location[each.key].result.location)
+  handler          = each.value.handler
+  runtime          = "java17"
+  memory_size      = 1024
+  timeout          = 30
+}
+
+resource "aws_cloudwatch_event_rule" "update_events_hourly" {
+  name                = "${local.application_id}_update_events_hourly"
+  description         = "Trigger the UpdateEventsHandler Lambda function every hour"
+  schedule_expression = "rate(1 hour)"
+}
+
+resource "aws_cloudwatch_event_target" "update_events_lambda" {
+  rule      = aws_cloudwatch_event_rule.update_events_hourly.name
+  target_id = "UpdateEventsHandler"
+  arn       = aws_lambda_function.lambda["update_events"].arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowEventBridgeInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda["update_events"].function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.update_events_hourly.arn
+}
+
+
+resource "aws_lambda_permission" "api_gateway" {
+  for_each = local.endpoints
+
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda[each.key].function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.event_calendar.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_rest_api" "event_calendar" {
+  name = "${local.application_id}_gateway"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  for_each = local.endpoints
+
+  rest_api_id = aws_api_gateway_rest_api.event_calendar.id
+  parent_id   = aws_api_gateway_rest_api.event_calendar.root_resource_id
+  path_part   = each.value.path
+}
+
+resource "aws_api_gateway_method" "method" {
+  for_each = local.endpoints
+
+  rest_api_id   = aws_api_gateway_rest_api.event_calendar.id
+  resource_id   = aws_api_gateway_resource.resource[each.key].id
+  http_method   = each.value.method
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  for_each = local.endpoints
+
+  rest_api_id             = aws_api_gateway_rest_api.event_calendar.id
+  resource_id             = aws_api_gateway_resource.resource[each.key].id
+  http_method             = aws_api_gateway_method.method[each.key].http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda[each.key].invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "event_calendar" {
+  rest_api_id = aws_api_gateway_rest_api.event_calendar.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.resource,
+      aws_api_gateway_method.method,
+      aws_api_gateway_integration.integration,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.event_calendar.id
+  rest_api_id   = aws_api_gateway_rest_api.event_calendar.id
+  stage_name    = "prod"
+}
+
+resource "aws_acm_certificate" "event_calendar" {
+  provider          = aws.us_east_1
+  domain_name       = "api.event-calendar.jordansimsmith.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_domain_name" "event_calendar" {
+  domain_name     = aws_acm_certificate.event_calendar.domain_name
+  certificate_arn = aws_acm_certificate.event_calendar.arn
+}
+
+resource "aws_api_gateway_base_path_mapping" "event_calendar" {
+  api_id      = aws_api_gateway_rest_api.event_calendar.id
+  stage_name  = aws_api_gateway_stage.prod.stage_name
+  domain_name = aws_api_gateway_domain_name.event_calendar.domain_name
+}
