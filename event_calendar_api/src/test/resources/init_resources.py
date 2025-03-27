6359c9eabd2c185ba78ce18687b13252a1a29676
Jordan Sim-Smith <jordansimsmith@canva.com>
2025-03-27 13:19:18 +1300

    Add e2e test for event calendar

---
+++ event_calendar_api/src/test/resources/init_resources.py
@@ -0,0 +1,87 @@
+import boto3
+import json
+
+region_name = "ap-southeast-2"
+endpoint_url = "http://localhost:4566"
+
+iam_client = boto3.client("iam", endpoint_url=endpoint_url, region_name=region_name)
+lambda_client = boto3.client(
+    "lambda", endpoint_url=endpoint_url, region_name=region_name
+)
+dynamodb_client = boto3.client(
+    "dynamodb", endpoint_url=endpoint_url, region_name=region_name
+)
+events_client = boto3.client(
+    "events", endpoint_url=endpoint_url, region_name=region_name
+)
+
+table_name = "event_calendar"
+dynamodb_client.create_table(
+    TableName=table_name,
+    AttributeDefinitions=[
+        {"AttributeName": "pk", "AttributeType": "S"},
+        {"AttributeName": "sk", "AttributeType": "S"},
+    ],
+    KeySchema=[
+        {"AttributeName": "pk", "KeyType": "HASH"},
+        {"AttributeName": "sk", "KeyType": "RANGE"},
+    ],
+    BillingMode="PAY_PER_REQUEST",
+)
+dynamodb_client.get_waiter("table_exists").wait(TableName=table_name)
+
+role_name = "lambda-execution-role"
+policy_name = "lambda-execution-policy"
+role_arn = f"arn:aws:iam::000000000000:role/{role_name}"
+assume_role_policy_document = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {"Service": "lambda.amazonaws.com"},
+            "Action": "sts:AssumeRole",
+        }
+    ],
+}
+iam_client.create_role(
+    RoleName=role_name, AssumeRolePolicyDocument=json.dumps(assume_role_policy_document)
+)
+policy_document = {
+    "Version": "2012-10-17",
+    "Statement": [{"Effect": "Allow", "Action": "*", "Resource": "*"}],
+}
+iam_client.put_role_policy(
+    RoleName=role_name,
+    PolicyName=policy_name,
+    PolicyDocument=json.dumps(policy_document),
+)
+
+configs = [
+    {
+        "function_name": "update_events_handler",
+        "handler_name": "com.jordansimsmith.eventcalendar.UpdateEventsHandler",
+        "zip_file": "update-events-handler_deploy.jar",
+    },
+    {
+        "function_name": "get_calendar_subscription_handler",
+        "handler_name": "com.jordansimsmith.eventcalendar.GetCalendarSubscriptionHandler",
+        "zip_file": "get-calendar-subscription-handler_deploy.jar",
+    },
+]
+
+for config in configs:
+    with open(f"/opt/code/localstack/{config['zip_file']}", "rb") as f:
+        zip_file_bytes = f.read()
+
+    function_arn = lambda_client.create_function(
+        FunctionName=config["function_name"],
+        Runtime="java17",
+        Role=role_arn,
+        Handler=config["handler_name"],
+        Code={"ZipFile": zip_file_bytes},
+        Timeout=120,
+        MemorySize=1024,
+    )["FunctionArn"]
+    lambda_client.get_waiter("function_active_v2").wait(
+        FunctionName=config["function_name"]
+    )
