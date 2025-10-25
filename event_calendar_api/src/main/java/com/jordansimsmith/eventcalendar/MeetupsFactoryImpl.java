1f358b848bdb7e694d7d6882331f5bc5fc906dc2
Jordan Sim-Smith <jordansimsmith@canva.com>
2025-10-25 20:23:20 +1300

    Fix issue with incorrect meetup url

--- event_calendar_api/src/main/java/com/jordansimsmith/eventcalendar/MeetupsFactoryImpl.java
+++ event_calendar_api/src/main/java/com/jordansimsmith/eventcalendar/MeetupsFactoryImpl.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class MeetupsFactoryImpl implements MeetupsFactory {
   private static final MeetupGroup ENKAI =
       new MeetupGroup(
-          URI.create("https://www.meetup.com/auckland-japanese-english-exchange-enkai-"));
+          URI.create("https://www.meetup.com/auckland-japanese-english-exchange-enkai-縁会"));
 
   @Override
   public List<MeetupGroup> findMeetupGroups() {
