daba735659cba9795b91e75c0bdbb1b7a4fe2537
Jordan Sim-Smith <jordansimsmith@canva.com>
2026-06-01 19:43:10 +1200

    Add JSON response templates to API Gateway error responses

--- japanese_dictionary_api/infra/main.tf
+++ japanese_dictionary_api/infra/main.tf
@@ -387,6 +387,10 @@ resource "aws_api_gateway_gateway_response" "default_4xx" {
   rest_api_id   = aws_api_gateway_rest_api.japanese_dictionary.id
   response_type = "DEFAULT_4XX"
 
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
+
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin"  = "'${local.cors_origins[0]}'"
     "gatewayresponse.header.Access-Control-Allow-Headers" = "'Authorization,Content-Type'"
@@ -397,6 +401,10 @@ resource "aws_api_gateway_gateway_response" "default_4xx" {
 resource "aws_api_gateway_gateway_response" "default_5xx" {
   rest_api_id   = aws_api_gateway_rest_api.japanese_dictionary.id
   response_type = "DEFAULT_5XX"
+
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
 
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin"  = "'${local.cors_origins[0]}'"
--- packing_list_api/infra/main.tf
+++ packing_list_api/infra/main.tf
@@ -352,6 +352,10 @@ resource "aws_api_gateway_gateway_response" "default_4xx" {
   rest_api_id   = aws_api_gateway_rest_api.packing_list.id
   response_type = "DEFAULT_4XX"
 
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
+
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin"  = "'${local.cors_origins[0]}'"
     "gatewayresponse.header.Access-Control-Allow-Headers" = "'Authorization,Content-Type'"
@@ -362,6 +366,10 @@ resource "aws_api_gateway_gateway_response" "default_4xx" {
 resource "aws_api_gateway_gateway_response" "default_5xx" {
   rest_api_id   = aws_api_gateway_rest_api.packing_list.id
   response_type = "DEFAULT_5XX"
+
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
 
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin"  = "'${local.cors_origins[0]}'"
