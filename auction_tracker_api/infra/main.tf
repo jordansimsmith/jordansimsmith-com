af2e925dfe9ef1a989ed81231762f2a839f0e7a9
Jordan Sim-Smith <jordansimsmith@gmail.com>
2026-09-25 21:50:14 +1200

    Preserve Scheduler context tokens in SQS payloads

--- auction_tracker_api/infra/main.tf
+++ auction_tracker_api/infra/main.tf
@@ -336,15 +336,13 @@ resource "aws_scheduler_schedule" "update_search" {
   target {
     arn      = "arn:aws:scheduler:::aws-sdk:sqs:sendMessage"
     role_arn = aws_iam_role.jobs_scheduler.arn
-    input = jsonencode({
-      QueueUrl = aws_sqs_queue.jobs.url
-      MessageBody = jsonencode({
-        job_type     = "update_search"
-        search_id    = each.value
-        scheduled_at = "<aws.scheduler.scheduled-time>"
-      })
-      MessageGroupId = "auction-tracker"
-    })
+    input    = <<-JSON
+      {
+        "QueueUrl": "${aws_sqs_queue.jobs.url}",
+        "MessageBody": "{\"job_type\":\"update_search\",\"search_id\":\"${each.value}\",\"scheduled_at\":\"<aws.scheduler.scheduled-time>\"}",
+        "MessageGroupId": "auction-tracker"
+      }
+    JSON
 
     retry_policy {
       maximum_event_age_in_seconds = 3600
@@ -368,14 +366,13 @@ resource "aws_scheduler_schedule" "send_digest" {
   target {
     arn      = "arn:aws:scheduler:::aws-sdk:sqs:sendMessage"
     role_arn = aws_iam_role.jobs_scheduler.arn
-    input = jsonencode({
-      QueueUrl = aws_sqs_queue.jobs.url
-      MessageBody = jsonencode({
-        job_type     = "send_digest"
-        scheduled_at = "<aws.scheduler.scheduled-time>"
-      })
-      MessageGroupId = "auction-tracker"
-    })
+    input    = <<-JSON
+      {
+        "QueueUrl": "${aws_sqs_queue.jobs.url}",
+        "MessageBody": "{\"job_type\":\"send_digest\",\"scheduled_at\":\"<aws.scheduler.scheduled-time>\"}",
+        "MessageGroupId": "auction-tracker"
+      }
+    JSON
 
     retry_policy {
       maximum_event_age_in_seconds = 3600
